d30ab1e443be786d50644bbcfe01eab2176e9070
andressamesquita <rib.mesq.andressa72@gmail.com>
2021-03-22 17:36:30 -0300

    Consulta com jpql

--- src/main/java/br/com/alura/loja/teste/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/teste/CadastroDeProduto.java
@@ -1,6 +1,7 @@
 package br.com.alura.loja.teste;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
@@ -13,17 +14,30 @@ import br.com.alura.loja.util.JPAUtil;
 public class CadastroDeProduto {
 	
 	public static void main(String[] args) {
-		Categoria CELULARES = new Categoria("CELULARES");
-		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), CELULARES);
-				
-		EntityManager em = JPAUtil.getEntityManager();
+		cadastrarProduto();
 		
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		
+		Produto p = produtoDao.buscarPorId(1l);
+		System.out.println(p.getPreco());
+		
+		List<Produto> todos = produtoDao.buscarTodos();
+		todos.forEach(p2 -> System.out.println(p.getNome()));
+
+	}
+
+	private static void cadastrarProduto() {
+		Categoria celulares = new Categoria("CELULARES");
+		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares );
+		
+		EntityManager em = JPAUtil.getEntityManager();
 		ProdutoDao produtoDao = new ProdutoDao(em);
 		CategoriaDao categoriaDao = new CategoriaDao(em);
 		
 		em.getTransaction().begin();
 		
-		categoriaDao.cadastrar(CELULARES);
+		categoriaDao.cadastrar(celulares);
 		produtoDao.cadastrar(celular);
 		
 		em.getTransaction().commit();
