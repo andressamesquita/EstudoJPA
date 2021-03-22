ac4db3d573a6c73d63b0dad1539141604a0cff04
andressamesquita <rib.mesq.andressa72@gmail.com>
2021-03-22 15:11:30 -0300

    Flexibilidade: retirando enum e mapeando um relacionamento ManyToOne entre Produto e Categoria

--- src/main/java/br/com/alura/loja/teste/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/teste/CadastroDeProduto.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
 
+import br.com.alura.loja.dao.CategoriaDao;
 import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
@@ -12,14 +13,19 @@ import br.com.alura.loja.util.JPAUtil;
 public class CadastroDeProduto {
 	
 	public static void main(String[] args) {
-		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), Categoria.CELULARES);
+		Categoria CELULARES = new Categoria("CELULARES");
+		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), CELULARES);
 				
 		EntityManager em = JPAUtil.getEntityManager();
 		
-		ProdutoDao dao = new ProdutoDao(em);
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		CategoriaDao categoriaDao = new CategoriaDao(em);
 		
 		em.getTransaction().begin();
-		dao.cadastrar(celular);
+		
+		categoriaDao.cadastrar(CELULARES);
+		produtoDao.cadastrar(celular);
+		
 		em.getTransaction().commit();
 		em.close();
 	}
