a299960289855c20ecbbbb12d9fa06f55120ede2
andressamesquita <rib.mesq.andressa72@gmail.com>
2021-03-23 11:31:03 -0300

    Consulta usando apenas um atributo

--- src/main/java/br/com/alura/loja/teste/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/teste/CadastroDeProduto.java
@@ -24,6 +24,9 @@ public class CadastroDeProduto {
 		
 		List<Produto> todos = produtoDao.buscarPorNomeDaCategoria("CELULARES");
 		todos.forEach(p2 -> System.out.println(p.getNome()));
+		
+		/*BigDecimal precoDoProduto = produtoDao.buscarPrecoDoProdutoComNome("Xiaomi Redmi");
+		System.out.println("Preço do Produto: " + precoDoProduto);*/
 
 	}
 
